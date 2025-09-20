57779035df49f5e0eacfaaa546f8b1edc78a7afb
asha24workofficial <asha24workofficial@gmail.com>
2025-09-20 17:54:45 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  // Existing content...
-theme: {
-  extend: {
-    fontFamily: {
-      'manrope': ['Manrope', 'sans-serif'],
-    },
-    colors: { // Add this block
-      customBlue: '#3EA0C6',
+  theme: {
+    extend: {
+      fontFamily: {
+        'manrope': ['Manrope', 'sans-serif'],
+      },
+      colors: {
+        customBlue: '#3EA0C6',
+        customDarkBlue: '#20576D',
+      },
     },
   },
-},
-// Existing content...
-
   plugins: [],
 };
