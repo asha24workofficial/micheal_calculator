dead97a32ded8c871d2273c30dde59641ddb632c
asha24workofficial <asha24workofficial@gmail.com>
2025-09-20 17:54:11 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'manrope': ['Manrope', 'sans-serif'],
-      },
+  // Existing content...
+theme: {
+  extend: {
+    fontFamily: {
+      'manrope': ['Manrope', 'sans-serif'],
+    },
+    colors: { // Add this block
+      customBlue: '#3EA0C6',
     },
   },
+},
+// Existing content...
+
   plugins: [],
 };
